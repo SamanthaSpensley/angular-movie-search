3c09233b557f0acac5e57b563bdd7b62807a0efe
Samantha Spensley <spenslsm@gmail.com>
2016-10-19 10:44:55 -0600

    movie details

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ app.config(function($routeProvider, $locationProvider) {
         templateUrl: 'partials/home.html',
         controller: 'HomeController'
       })
-      .otherwise({ redirectTo: '/' })
+      // .otherwise({ redirectTo: '/' })
       $locationProvider.html5Mode(true);
 });
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/view/:imdbID*', {
+    templateUrl: 'partials/movie.html',
+    controller: 'MovieController'
+  })
+  $locationProvider.html5Mode(true);
+})
--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,5 @@
 app.controller('HomeController', function($scope, $http, $route) {
   $scope.view = {};
-  $scope.view.message = "Welcome!"
 
   $scope.searchMovie = function() {
     var input = $scope.view.searchInput;
@@ -15,5 +14,20 @@ app.controller('HomeController', function($scope, $http, $route) {
       $scope.view.error = error.status;
     })
   }
-
 });
+
+app.controller('MovieController', function($scope, $http, $route) {
+  $scope.view = {};
+  var movieId = $route.current.params.imdbID;
+  var url = 'http://www.omdbapi.com/?i=' + movieId;
+  console.log(url);
+
+  $http.get(url)
+  .success(function(data) {
+    $scope.view.movieDetails = data;
+    console.log(data);
+  })
+  .catch(function(error) {
+    $scope.view.error = error.status;
+  })
+})
